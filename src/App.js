77e88c4ad8965498d0e2d2aebcbcca2bbf0ffec3
Randila Premarathne <randilamenukapremarathne@gmail.com>
2024-06-03 18:17:54 +0530

    Setup navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {Component} from 'react';
 import {BrowserRouter} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
+      <Navbar />
     </div>
     </BrowserRouter>
   );
