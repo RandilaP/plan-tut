15deb1dd84d63d3af5d7dd90b14a5aff042f5bad
Randila Premarathne <randilamenukapremarathne@gmail.com>
2024-06-03 18:59:43 +0530

    Setup dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import {Component} from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+      </Routes>
     </div>
     </BrowserRouter>
   );
